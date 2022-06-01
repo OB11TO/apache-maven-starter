16bf65a8150b8a9fecfabc19f1095a4b4583df9a
OB11TO <OB11TO61@yandex.ru>
2022-06-01 12:46:27 +0300

    init commit

---
+++ src/main/java/com/ob11to/App.java
@@ -0,0 +1,13 @@
+package com.ob11to;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
