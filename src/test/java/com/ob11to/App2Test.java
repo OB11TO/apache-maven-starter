f83006f4d1265f8d3babcfbb61ea441e0ed02ceb
OB11TO <OB11TO61@yandex.ru>
2022-06-02 14:13:09 +0300

    Maven. 13. Default Lifecycle. Test phase. Surefire plugin

---
+++ src/test/java/com/ob11to/App2Test.java
@@ -0,0 +1,13 @@
+package com.ob11to;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class App2Test {
+    @Test
+    public void shouldAnswerWithTrue2() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/com/ob11to/AppTestCase.java
@@ -0,0 +1,13 @@
+package com.ob11to;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTestCase {
+    @Test
+    public void shouldAnswerWithTrue3()
+    {
+        assertTrue( true );
+    }
+}
