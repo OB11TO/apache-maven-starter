c08b309510b8b0119d939b14029e27ab284698fb
OB11TO <OB11TO61@yandex.ru>
2022-06-07 13:52:45 +0300

    Maven. 26. Executable jar with dependencies

--- service/src/main/java/com/ob11to/service/AppRunner.java
+++ service/src/main/java/com/ob11to/service/AppRunner.java
@@ -1,9 +1,11 @@
 package com.ob11to.service;
 
+import com.ob11to.service.service.UserService;
+
 public class AppRunner {
     public static void main(String[] args) {
-//        var userService = new UserService();
-//        System.out.println(userService.getUser(25L));
+        var userService = new UserService();
+        System.out.println(userService.getUser(25L));
         System.out.println("Hello");
     }
 }
