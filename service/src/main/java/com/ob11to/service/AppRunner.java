419d19ad2a5ca310e1f794b3cf17547d8da1ab71
OB11TO <OB11TO61@yandex.ru>
2022-06-07 12:43:55 +0300

    Maven. 25. Executable jar

---
+++ service/src/main/java/com/ob11to/service/AppRunner.java
@@ -0,0 +1,9 @@
+package com.ob11to.service;
+
+public class AppRunner {
+    public static void main(String[] args) {
+//        var userService = new UserService();
+//        System.out.println(userService.getUser(25L));
+        System.out.println("Hello");
+    }
+}
